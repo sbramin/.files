1a90b2502e5072ef63c422f4aaa077913a4df6d6
sbr <sascha.ramin@sky.uk>
2017-02-18 15:07:11 +0000

    added vim plug installer

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #/bin/sh
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s ~/.files/bashrc ~/.bashrc
 ln -s ~/.files/vimrc ~/.vimrc
