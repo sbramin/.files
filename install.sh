5cf73b28ae3a04602d1ca7deb983f90aaaf8cbdf
sbramin <s@obscurity.xyz>
2018-07-18 09:39:14 +0100

    Cleanup, add os specific files, replace lenny with leno

--- install.sh
+++ install.sh
@@ -2,11 +2,24 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s ~/.files/bashrc ~/.bashrc
-ln -s ~/.files/vimrc ~/.vimrc
-ln -s ~/.files/tmux.conf ~/.tmux.conf
-ln -s ~/.files/Xmodmap ~/.Xmodmap
-ln -s ~/.files/gitconfig ~/.gitconfig
+host=`hostname -s`
+os=`uname`
+
+ln -sf ~/.files/vimrc ~/.vimrc
+ln -sf ~/.files/tmux.conf ~/.tmux.conf
+ln -sf ~/.files/Xmodmap ~/.Xmodmap
+ln -sf ~/.files/gitconfig ~/.gitconfig
+ln -sf ~/.files/muttrc~/.muttrc
+
+if [ "$os" == "OpenBSD" ]; then
+	ln -sf ~/.files/$host/xsesssion ~/.xsession
+	ln -sf ~/.files/$host/Xdefaults ~/.Xdefaults
+fi
+
+
+if [ "$os" == "Arch" ]; then
+	ln -sf ~/.files/zshrc ~/.zshrc
+fi
 
 
 # In vim type :PlugInstall
