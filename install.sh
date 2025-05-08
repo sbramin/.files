c13721fe5cb25e6ff846f81f24535917701f177b
sbr <sbr@doe.8by3.net>
2025-05-08 10:13:39 +0200

    overall for 2025, obsd only

--- install.sh
+++ install.sh
@@ -1,27 +1,8 @@
 #/bin/sh
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-host=`hostname -s`
-os=`uname`
-
-ln -sf ~/.files/vimrc ~/.vimrc
 ln -sf ~/.files/tmux.conf ~/.tmux.conf
+ln -sf ~/.files/cmwrc.conf ~/.cwmrc.conf
 ln -sf ~/.files/Xmodmap ~/.Xmodmap
-ln -sf ~/.files/gitconfig ~/.gitconfig
-ln -sf ~/.files/muttrc~/.muttrc
-
-if [ "$os" == "OpenBSD" ]; then
-	ln -sf ~/.files/$host/xsesssion ~/.xsession
-	ln -sf ~/.files/$host/Xdefaults ~/.Xdefaults
-fi
-
-
-if [ "$os" == "Arch" ]; then
-	ln -sf ~/.files/zshrc ~/.zshrc
-fi
-
-
-# In vim type :PlugInstall
-# Then :GoInstallBinaries
+ln -sf ~/.files/xsesssion ~/.xsession
+ln -sf ~/.files/Xdefaults ~/.Xdefaults
 
